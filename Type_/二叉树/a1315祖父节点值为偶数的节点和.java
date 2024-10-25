a6a39cec052779e8c0759b3ccee00390620aaa0b
Yuse1 <1623500565@qq.com>
2024-10-25 14:00:18 +0800

    MID - 二叉树 - 向前看和向后看的区别

--- Type_/二叉树/a1315祖父节点值为偶数的节点和.java
+++ Type_/二叉树/a1315祖父节点值为偶数的节点和.java
@@ -8,62 +8,56 @@ import Common.TreeNode;
  */
 public class a1315祖父节点值为偶数的节点和 {
 
-    public int sum = 0;
+    class Solution {
+        public int sum = 0;
 
-    public int sumEvenGrandparent(TreeNode root) {
-        //traverse1(root, null, null);
-        traverse2(root);
-        return sum;
-    }
-
-    /**
-     * 往前面看
-     *
-     * @param root
-     * @param parent
-     * @param grandpa
-     */
-    public void traverse1(TreeNode root, TreeNode parent, TreeNode grandpa) {
-        if (root == null)
-            return;
-
-        if (grandpa != null && (grandpa.val & 1) == 0) {
-            sum += root.val;
+        public int sumEvenGrandparent(TreeNode root) {
+            //traverse1(root, null, null);
+            traverse2(root);
+            return sum;
         }
-        traverse1(root.left, root, parent);
-        traverse1(root.right, root, parent);
-    }
 
-    /**
-     * 往后面看
-     *
-     * @param root
-     */
-    public void traverse2(TreeNode root) {
-        if (root == null)
-            return;
-        if ((root.val & 1) == 0) {
-            TreeNode lNode = root.left;
-            if (lNode != null) {
-                TreeNode llNode = lNode.left;
-                TreeNode lrNode = lNode.right;
-                if (llNode != null)
-                    sum += llNode.val;
-                if (lrNode != null)
-                    sum += lrNode.val;
+        /**
+         * 往前面看
+         *
+         * @param root
+         * @param parent
+         * @param grandpa
+         */
+        public void traverse1(TreeNode root, TreeNode parent, TreeNode grandpa) {
+            if (root == null) return;
+            if (grandpa != null && (grandpa.val & 1) == 0) {
+                sum += root.val;
             }
-            TreeNode rNode = root.right;
-            if (rNode != null) {
-                TreeNode rlNode = rNode.left;
-                TreeNode rrNode = rNode.right;
-                if (rlNode != null)
-                    sum += rlNode.val;
-                if (rrNode != null)
-                    sum += rrNode.val;
-            }
+            traverse1(root.left, root, parent);
+            traverse1(root.right, root, parent);
         }
-        traverse2(root.left);
-        traverse2(root.right);
-    }
 
+        /**
+         * 往后面看
+         *
+         * @param root
+         */
+        public void traverse2(TreeNode root) {
+            if (root == null) return;
+            if ((root.val & 1) == 0) {
+                TreeNode lNode = root.left;
+                if (lNode != null) {
+                    TreeNode llNode = lNode.left;
+                    TreeNode lrNode = lNode.right;
+                    if (llNode != null) sum += llNode.val;
+                    if (lrNode != null) sum += lrNode.val;
+                }
+                TreeNode rNode = root.right;
+                if (rNode != null) {
+                    TreeNode rlNode = rNode.left;
+                    TreeNode rrNode = rNode.right;
+                    if (rlNode != null) sum += rlNode.val;
+                    if (rrNode != null) sum += rrNode.val;
+                }
+            }
+            traverse2(root.left);
+            traverse2(root.right);
+        }
+    }
 }
