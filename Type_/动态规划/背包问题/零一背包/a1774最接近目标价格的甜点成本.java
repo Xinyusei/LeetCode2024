5cec936d709af2ab72ec4019578dddd10bf1db64
Yuse1 <1623500565@qq.com>
2024-12-07 20:06:33 +0800

    零一背包的问题转换

---
+++ Type_/动态规划/背包问题/零一背包/a1774最接近目标价格的甜点成本.java
@@ -0,0 +1,35 @@
+package Type_.动态规划.背包问题.零一背包;
+
+/**
+ * @Author: ZJX
+ * @Date: 2024/12/07
+ * @Description:
+ */
+public class a1774最接近目标价格的甜点成本 {
+    class Solution {
+
+        public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+            int n = baseCosts.length, m = toppingCosts.length;
+            int ans = -1, minDiff = Integer.MAX_VALUE;
+            for (int i = 0; i < n; i++) {
+                for (int state = 0; state < (int) Math.pow(3, m); state++) {
+                    int cost = baseCosts[i];
+                    int mask = state;
+                    for (int k = 0; k < m; k++) {
+                        cost += (mask % 3) * toppingCosts[k];
+                        mask /= 3;
+                    }
+                    int diff = Math.abs(cost - target);
+                    if (diff < minDiff) {
+                        minDiff = diff;
+                        ans = cost;
+                    } else if (diff == minDiff && cost < ans) {
+                        ans = cost;
+                    }
+
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ Type_/动态规划/背包问题/零一背包/a3180执行操作可获得的最大总奖励I.java
@@ -0,0 +1,35 @@
+package Type_.动态规划.背包问题.零一背包;
+
+import java.util.Arrays;
+
+/**
+ * @Author: ZJX
+ * @Date: 2024/12/07
+ * @Description:
+ */
+public class a3180执行操作可获得的最大总奖励I {
+    class Solution {
+        public int maxTotalReward(int[] s) {
+            Arrays.sort(s);
+            int n = s.length;
+            //最大值为 mx ,想要全部加起来, 前面的和最大是 mx - 1 ，也就是说最大价值就是 2 * mx - 1,即 背包容量 <= 2 * mx
+            int mx = s[n - 1];
+            boolean[] f = new boolean[2 * mx];
+            //在 i 个数中挑选，总奖励j 是否可以获得
+            //base case
+            f[0] = true;
+            for (int i = 1; i < n; i++) {
+                int v = s[i]; //当前数的值
+                for (int j = 2 * mx - 1; j >= v; j--) {
+                    f[j] |= f[j - v];
+                }
+            }
+            int ans = 0;
+            for (int i = 0; i < f.length; i++) {
+                if (f[i])
+                    ans = i;
+            }
+            return ans;
+        }
+    }
+}
