7c9b3bc2d0d48d890149a038434ece553ae87768
Yuse1 <1623500565@qq.com>
2024-12-11 17:29:58 +0800

    同样的思路 - 求最长严格递增子序列需要二分找到大于或等于当前元素的元素位置（即 lower_bound）； 求最长不降子序列需要二分找到大于当前元素的元素位置（即upper_bound）。

--- Type_/动态规划/经典线性DP/最长递增子序列LIS/a1964找出到每个位置为止最长的有效障碍赛跑路线.java
+++ Type_/动态规划/经典线性DP/最长递增子序列LIS/a1964找出到每个位置为止最长的有效障碍赛跑路线.java
@@ -11,6 +11,8 @@ import java.util.List;
 public class a1964找出到每个位置为止最长的有效障碍赛跑路线 {
     class Solution {
         public int[] longestObstacleCourseAtEachPosition(int[] obstacles) {
+            //求最长严格递增子序列需要二分找到大于或等于当前元素的元素位置（即lower_bound）；
+            //求最长不降子序列需要二分找到大于当前元素的元素位置（即upper_bound）。
             //1 2 3
             //LIS - 用灵神的思路
             //g[i] 表示 : 长度为 i + 1 的 非递减子序列的 末尾元素的最小值
@@ -18,7 +20,7 @@ public class a1964找出到每个位置为止最长的有效障碍赛跑路线 {
             List<Integer> g = new ArrayList<>();
             int[] f = new int[n];
             for (int i = 0; i < n; i++) {
-                int idx = lowerBound(g, obstacles[i]);
+                int idx = upperBound(g, obstacles[i]);
                 //说明是新增的元素
                 if (idx != g.size())
                     g.set(idx, obstacles[i]);
@@ -29,13 +31,13 @@ public class a1964找出到每个位置为止最长的有效障碍赛跑路线 {
             return f;
         }
 
-        //找到 >= x 的最小索引
-        private int lowerBound(List<Integer> g, int x) {
+        //找到 > x 的最小索引
+        private int upperBound(List<Integer> g, int x) {
             int lo = 0, hi = g.size();
             while (lo < hi) {
                 int mid = (hi - lo) / 2 + lo;
                 //[lo,mid) mid [mid + 1,hi)
-                if (g.get(mid) >= x)
+                if (g.get(mid) > x)
                     hi = mid;
                 else
                     lo = mid + 1;
