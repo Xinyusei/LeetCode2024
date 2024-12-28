a617d0a915a4b24afd4af32c20b8aac1f4672072
Yuse1 <1623500565@qq.com>
2024-12-28 17:41:21 +0800

    几道矩阵相关的题目 - 都很重要

---
+++ 面试150/a36有效的数独.java
@@ -0,0 +1,103 @@
+package 面试150;
+
+/**
+ * @Author: ZJX
+ * @Date: 2024/12/28
+ * @Description:
+ */
+public class a36有效的数独 {
+
+    /**
+     * 哈希表计数 - todo
+     */
+    class S2 {
+        class Solution {
+            public boolean isValidSudoku(char[][] board) {
+                int n = board.length;
+                int[][] rowCnt = new int[n][10];
+                int[][] colCnt = new int[n][10];
+                int[][][] subCnt = new int[n / 3][n / 3][10];
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if (board[i][j] == '.')
+                            continue;
+                        int v = board[i][j] - '0';
+                        rowCnt[i][v]++;
+                        colCnt[j][v]++;
+                        subCnt[i / 3][j / 3][v]++;
+                        if (rowCnt[i][v] > 1 || colCnt[j][v] > 1 || subCnt[i / 3][j / 3][v] > 1)
+                            return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+
+    /**
+     * 朴素解法
+     */
+    class S1 {
+        class Solution {
+            public boolean isValidSudoku(char[][] board) {
+                int n = board.length;
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        char v = board[i][j];
+                        //判断规则1是否满足
+                        if (!checkRow(v, board, i))
+                            return false;
+                        System.out.println("通过规则1");
+                        //判断规则2是否满足
+                        if (!checkCol(v, board, j))
+                            return false;
+                        System.out.println("通过规则2");
+
+                        if (!checkMatrix(v, board, i, j))
+                            return false;
+                        System.out.println("通过规则3");
+                        //判断规则3是否满足
+                    }
+                }
+                return true;
+            }
+
+            private boolean checkRow(char v, char[][] g, int row) {
+                int cnt = 0;
+                for (char c : g[row]) {
+                    if (c != '.' && c == v)
+                        cnt++;
+                    if (cnt > 1)
+                        return false;
+                }
+                return true;
+            }
+
+            private boolean checkCol(char v, char[][] g, int col) {
+                int cnt = 0;
+                for (char[] cs : g) {
+                    if (cs[col] != '.' && cs[col] == v)
+                        cnt++;
+                    if (cnt > 1)
+                        return false;
+                }
+                return true;
+            }
+
+            private boolean checkMatrix(int v, char[][] g, int i, int j) {
+                int startRow = i / 3, startCol = j / 3, cnt = 0;
+                for (int row = startRow * 3; row < startRow * 3 + 3; row++) {
+                    for (int col = startCol * 3; col < startCol * 3 + 3; col++) {
+                        if (g[row][col] != '.' && g[row][col] == v)
+                            cnt++;
+                        if (cnt > 1)
+                            return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ 面试150/a48旋转图像.java
@@ -0,0 +1,83 @@
+package 面试150;
+
+/**
+ * @Author: ZJX
+ * @Date: 2024/12/28
+ * @Description:
+ */
+public class a48旋转图像 {
+    /**
+     * 找规律 - 必须掌握
+     */
+    class S1 {
+        class Solution {
+            public void rotate(int[][] matrix) {
+                int n = matrix.length;
+                //i -> j
+                //j -> n - 1 - i
+
+                //( i ,j ) -> (j, n - 1 - i)
+                //(j,n - 1 - i) -> (n - 1 - i, n - 1 - j)
+                //(n - 1 - i, n - 1 - j) -> (n - 1 - j, i)
+                //(n - 1 - j, i) -> (i, j)
+                for (int i = 0; i < n / 2; i++) {
+                    for (int j = 0; j < (n + 1) / 2; j++) {
+                        int temp = matrix[i][j]; //1
+                        //以下是逆时针旋转
+                        /*matrix[i][j] = matrix[j][n - 1 - i]; //
+                        matrix[j][n - 1 - i] = matrix[n - 1 - i][n - 1 - j];
+                        matrix[n - 1 - i][n - 1 - j] = matrix[n - 1 - j][i];
+                        matrix[n - 1 - j][i] = temp;*/
+                        //以下是顺时针旋转
+                        matrix[i][j] = matrix[n - 1 - j][i];
+                        matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                        matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                        matrix[j][n - 1 - i] = temp;
+                    }
+                }
+                //1 2 3
+                //4 5 6
+                //7 8 9
+            }
+        }
+    }
+
+    /**
+     * 旋转的经典思路 - 翻转全部 + 翻转部分
+     */
+    class S2 {
+        class Solution {
+            public void rotate(int[][] matrix) {
+                //先按 正对角线翻转 再每行翻转
+
+                int n = matrix.length;
+                //1.按对角线翻转
+                for (int i = 0; i < n; i++) {
+                    for (int j = i; j < n; j++) {
+                        int temp = matrix[i][j];
+                        matrix[i][j] = matrix[j][i];
+                        matrix[j][i] = temp;
+                    }
+                }
+                //1 4 7
+                //2 5 8
+                //3 6 9
+                //2.每一行再翻转
+                for (int[] row : matrix) {
+                    reverse(row);
+                }
+            }
+
+            public void reverse(int[] row) {
+                int lo = 0, hi = row.length - 1;
+                while (lo < hi) {
+                    int temp = row[lo];
+                    row[lo] = row[hi];
+                    row[hi] = temp;
+                    lo++;
+                    hi--;
+                }
+            }
+        }
+    }
+}
