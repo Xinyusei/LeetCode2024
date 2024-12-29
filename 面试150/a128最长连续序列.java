790aeee75e07f76da65b1b298b4e4897f534426e
Yuse1 <1623500565@qq.com>
2024-12-29 20:41:33 +0800

    没啥思路 - 还要多看

---
+++ 面试150/a128最长连续序列.java
@@ -0,0 +1,34 @@
+package 面试150;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Author: ZJX
+ * @Date: 2024/12/29
+ * @Description:
+ */
+public class a128最长连续序列 {
+    class Solution {
+        public int longestConsecutive(int[] nums) {
+            Set<Integer> record = new HashSet<>();
+            for (int x : nums) {
+                record.add(x);
+            }
+            int ans = 0;
+
+            for (int num : record) {
+                if (record.contains(num - 1))
+                    // num 不是连续子序列的第一个
+                    continue;
+                //num 是子序列的第一个
+                int curNum = num;
+                while (record.contains(curNum + 1)) {
+                    curNum++;
+                }
+                ans = Math.max(ans, curNum - num + 1);
+            }
+            return ans;
+        }
+    }
+}
