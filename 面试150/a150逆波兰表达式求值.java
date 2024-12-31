c52ec79005fd55f982217ce6a3d8c9975b76a39b
Yuse1 <1623500565@qq.com>
2024-12-31 16:40:09 +0800

    栈的应用 - 后缀表达式

---
+++ 面试150/a150逆波兰表达式求值.java
@@ -0,0 +1,45 @@
+package 面试150;
+
+import java.util.*;
+
+/**
+ * @Author: ZJX
+ * @Date: 2024/12/31
+ * @Description:
+ */
+public class a150逆波兰表达式求值 {
+    class S1 {
+        class Solution {
+            final static Set<String> r = new HashSet<>();
+
+            static {
+                r.add("+");
+                r.add("-");
+                r.add("*");
+                r.add("/");
+            }
+
+            //后缀表达式 -
+            public int evalRPN(String[] tokens) {
+                List<Integer> stack = new ArrayList<>();
+                for (String token : tokens) {
+                    if (!r.contains(token)) {
+                        stack.addLast(Integer.valueOf(token));
+                        continue;
+                    }
+                    //计算
+                    int second = stack.removeLast();
+                    int first = stack.removeLast();
+                    int res = switch (token) {
+                        case "+" -> first + second;
+                        case "-" -> first - second;
+                        case "*" -> first * second;
+                        default -> first / second;
+                    };
+                    stack.addLast(res);
+                }
+                return stack.removeLast();
+            }
+        }
+    }
+}
