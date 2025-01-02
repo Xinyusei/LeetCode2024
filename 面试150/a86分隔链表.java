9ea29af9da5e74f560d774127c3714d4770d9735
Yuse1 <1623500565@qq.com>
2025-01-02 14:11:13 +0800

    双指针 + 双dummy头结点

---
+++ 面试150/a86分隔链表.java
@@ -0,0 +1,31 @@
+package 面试150;
+
+import Common.ListNode;
+
+/**
+ * @Author: ZJX
+ * @Date: 2025/01/02
+ * @Description:
+ */
+public class a86分隔链表 {
+    class S1 {
+        class Solution {
+            public ListNode partition(ListNode head, int x) {
+                ListNode lo = new ListNode(-1024), hi = new ListNode(1024), p = lo, q = hi, cur = head;
+                while (cur != null) {
+                    if (cur.val < x) {
+                        p.next = cur;
+                        p = p.next;
+                    } else {
+                        q.next = cur;
+                        q = q.next;
+                    }
+                    cur = cur.next;
+                }
+                q.next = null;
+                p.next = hi.next;
+                return lo.next;
+            }
+        }
+    }
+}
