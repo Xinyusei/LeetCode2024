06957bc67c40d2089e37858ca4f31e52f776afc0
Yuse1 <1623500565@qq.com>
2025-02-10 22:35:28 +0800

    滑动窗口经典题目-todo

--- Type_/滑动窗口与双指针/不定长滑动窗口/不定长_求最小or最短/a76最小覆盖子串.java
+++ Type_/滑动窗口与双指针/不定长滑动窗口/不定长_求最小or最短/a76最小覆盖子串.java
@@ -2,6 +2,7 @@ package Type_.滑动窗口与双指针.不定长滑动窗口.不定长_求最小
 
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -41,4 +42,43 @@ public class a76最小覆盖子串 {
         }
         return len == s.length() + 1 ? "" : s.substring(startIndex, startIndex + len);
     }
+
+    class S1 {
+        class Solution {
+            public String minWindow(String s, String t) {
+                Map<Character, Integer> need = new HashMap<>();
+                for (char c : t.toCharArray()) {
+                    need.put(c, need.getOrDefault(c, 0) + 1);
+                }
+                Map<Character, Integer> curr = new HashMap<>();
+                int valid = 0;
+                int startIdx = 0, minLen = s.length() + 1;
+                for (int left = 0, right = 0; right < s.length(); right++) {
+                    char addCh = s.charAt(right);
+                    if (need.containsKey(addCh)) {
+                        curr.put(addCh, curr.getOrDefault(addCh, 0) + 1);
+                        if (need.get(addCh).equals(curr.get(addCh))) {
+                            valid++;
+                        }
+                    }
+
+                    while (valid == need.size()) {
+                        if (minLen > right - left + 1) {
+                            minLen = right - left + 1;
+                            startIdx = left;
+                        }
+                        char delCh = s.charAt(left);
+                        if (need.containsKey(delCh)) {
+                            if (need.get(delCh).equals(curr.get(delCh)))
+                                valid--;
+                            curr.put(delCh, curr.get(delCh) - 1);
+                        }
+                        left++;
+                    }
+                }
+                return minLen == s.length() + 1 ? "" : s.substring(startIdx, startIdx + minLen);
+            }
+        }
+    }
+
 }
