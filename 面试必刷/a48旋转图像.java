3011a8af8383a9c346ab314496ff173863beff81
Yuse1 <1623500565@qq.com>
2025-02-11 10:34:26 +0800

    旋转图像-找规律-todo

--- 面试必刷/a48旋转图像.java
+++ 面试必刷/a48旋转图像.java
@@ -6,6 +6,7 @@ package 面试必刷;
  * @Description:
  */
 public class a48旋转图像 {
+    //todo 
     /**
      * 找规律 - 必须掌握
      */
@@ -42,16 +43,17 @@ public class a48旋转图像 {
         }
     }
 
+
     /**
      * 旋转的经典思路 - 翻转全部 + 翻转部分
      */
-    class S2 {
+    class S2_1 {
         class Solution {
             public void rotate(int[][] matrix) {
-                //先按 正对角线翻转 再每行翻转
+                //先按 对角线翻转 再每行翻转
 
                 int n = matrix.length;
-                //1.按对角线翻转
+                //1. 按对角线翻转
                 for (int i = 0; i < n; i++) {
                     for (int j = i; j < n; j++) {
                         int temp = matrix[i][j];
@@ -62,14 +64,54 @@ public class a48旋转图像 {
                 //1 4 7
                 //2 5 8
                 //3 6 9
+
                 //2.每一行再翻转
                 for (int[] row : matrix) {
-                    reverse(row);
+                    reverse(row, 0, row.length - 1);
                 }
             }
 
-            public void reverse(int[] row) {
-                int lo = 0, hi = row.length - 1;
+            public void reverse(int[] row, int lo, int hi) {
+                while (lo < hi) {
+                    int temp = row[lo];
+                    row[lo] = row[hi];
+                    row[hi] = temp;
+                    lo++;
+                    hi--;
+                }
+            }
+        }
+    }
+
+    /**
+     *  旋转方式2 -> 翻转部分 +翻转全部
+     */
+    class S2_2 {
+        class Solution {
+            public void rotate(int[][] matrix) {
+                //1. 每行翻转
+                for (int[] row : matrix) {
+                    reverse(row, 0, row.length - 1);
+                }
+
+                //3 2 1
+                //6 5 4
+                //9 8 7
+
+                //2. 按主对角线翻转
+                int n = matrix.length;
+                for (int i = 0; i < n; i++) {
+                    for (int j = n - i - 1; j >= 0; j--) {
+                        int temp = matrix[i][j];
+                        //0,0 0,1 1,0
+                        //2,2 1,2 2,1
+                        matrix[i][j] = matrix[n - 1 - j][n - 1 - i];
+                        matrix[n - 1 - j][n - 1 - i] = temp;
+                    }
+                }
+            }
+
+            public void reverse(int[] row, int lo, int hi) {
                 while (lo < hi) {
                     int temp = row[lo];
                     row[lo] = row[hi];
@@ -81,3 +123,6 @@ public class a48旋转图像 {
         }
     }
 }
+
+
+
