e14531546d6f991ca29fce311150fc08a09ef75d
Yuse1 <1623500565@qq.com>
2025-02-18 15:50:33 +0800

    快速选择（两种方式）完善

--- Type_/数据结构/堆_优先队列/第K小or大/a215数组中的第K个最大元素.java
+++ Type_/数据结构/堆_优先队列/第K小or大/a215数组中的第K个最大元素.java
@@ -1,5 +1,7 @@
 package Type_.数据结构.堆_优先队列.第K小or大;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Random;
 
@@ -32,57 +34,102 @@ public class a215数组中的第K个最大元素 {
 
 
     /**
-     * 快速选择
+     * 快速选择 - 1
      */
-    class S2 {
-
+    class S2_1 {
         class Solution {
-            private final static Random random = new Random(System.currentTimeMillis());
+            private final static Random r = new Random();
 
             public int findKthLargest(int[] nums, int k) {
-                int n = nums.length;
-                int targetIdx = n - k;
+                int targetIdx = nums.length - k;
+                int lo = 0, hi = nums.length - 1;
 
-                int lo = 0;
-                int hi = nums.length - 1;
                 while (true) {
-                    int pivotIdx = partition(nums, lo, hi);
-                    if (pivotIdx == targetIdx)
-                        return nums[pivotIdx];
-                    else if (pivotIdx < targetIdx)
-                        lo = pivotIdx + 1;
+                    int partition = partition(nums, lo, hi);
+                    if (partition == targetIdx)
+                        return nums[partition];
+                    else if (partition < targetIdx)
+                        lo = partition + 1;
                     else
-                        hi = pivotIdx - 1;
+                        hi = partition - 1;
                 }
             }
 
-            //对 nums[lo..hi] 进行切分
-            private int partition(int[] nums, int l, int r) {
-                int randomIdx = l + random.nextInt(r - l + 1);
-                swap(nums, randomIdx, l);
+            //将数组划分为 <=pivot pivot >= pivot
+            public int partition(int[] nums, int lo, int hi) {
+                int idx = lo + r.nextInt(hi - lo + 1);
+                swap(nums, idx, lo);
+                int pivot = nums[lo];
+                int le = lo + 1, ge = hi;
 
-                int pivot = nums[l], le = l + 1, ge = r;
 
                 while (true) {
                     while (ge >= le && nums[ge] >= pivot) ge--;
                     while (le <= ge && nums[le] <= pivot) le++;
 
-                    if (le > ge)
+                    if(le > ge)
                         break;
                     swap(nums, le, ge);
-                    ge--;
                     le++;
-                }
-                swap(nums, l, ge);
+                    ge--;
 
+                }
+                swap(nums, lo, ge);
                 return ge;
             }
 
-            private void swap(int[] nums, int left, int right) {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
+            public void swap(int[] nums, int lo, int hi) {
+                int temp = nums[lo];
+                nums[lo] = nums[hi];
+                nums[hi] = temp;
+            }
+
+
+        }
+    }
+
+    /**
+     * 快速选择 - 2
+     */
+    class S2_2 {
+        class Solution {
+            private final static Random r = new Random();
+
+            public int findKthLargest(int[] nums, int k) {
+                List<Integer> ls = new ArrayList<>();
+                for (int num : nums) {
+                    ls.add(num);
+                }
+                return doFindKthLargest(ls, k);
+            }
+
+            public int doFindKthLargest(List<Integer> nums, int k) {
+                if (nums == null || nums.isEmpty())
+                    return 0;
+                int idx = r.nextInt(0, nums.size());
+                int pivot = nums.get(idx);
+
+                List<Integer> lt = new ArrayList<>();
+                List<Integer> gt = new ArrayList<>();
+
+                for (Integer num : nums) {
+                    if (num > pivot)
+                        gt.add(num);
+                    else if (num < pivot)
+                        lt.add(num);
+                }
+
+                //lt eq gt
+                //3 2 3
+                if (gt.size() >= k)
+                    return doFindKthLargest(gt, k);
+                else if (lt.size() + k > nums.size())
+                    return doFindKthLargest(lt, lt.size() + k - nums.size());
+                else
+                    return pivot;
             }
         }
     }
+
+
 }
